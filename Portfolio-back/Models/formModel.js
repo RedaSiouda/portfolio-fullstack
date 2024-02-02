2326cb70e5d4ed8812d3d22d3dcdc3ef634c6bd3
Siouda Reda <154610171+RedaSiouda@users.noreply.github.com>
2024-02-01 20:46:30 -0500

    Update formModel.js

--- Portfolio-back/Models/formModel.js
+++ Portfolio-back/Models/formModel.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-
+//initialisation of the form Schema
 const formSchema = new mongoose.Schema({
     email: String,
     message: String,
     timeStamp: String,
 });
 
+//create an instance of mongoose model
 const FormModel = mongoose.model('Form', formSchema);
 
 module.exports = FormModel;
