c87cdb4ede384902cb1fc25024217dcfc5a71700
Siouda Reda <154610171+RedaSiouda@users.noreply.github.com>
2024-02-01 20:48:00 -0500

    Update dbConnec.js

--- Portfolio-back/Services/dbConnec.js
+++ Portfolio-back/Services/dbConnec.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
+//function to connect to teh database (mongoDB)
 const connectToDatabase = async () => {
     try {
+        //check if URI is in .env
         if (!process.env.MONGO_URI) {
             throw new Error('Please provide the MONGO_URI in the .env file');
         }
 
+        //connection to the databse
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to the database...');
     } catch (err) {
         console.error('Failed to connect to the database:', err);
-        throw err; // Re-throw the error for consistent error handling
+        throw err; 
     }
 };
 
